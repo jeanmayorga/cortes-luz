b8b8eae321a6964273729e1017b87e5856d622ff
Jean Paul Mayorga <jpmayorga@outlook.com>
2024-11-23 12:05:01 -0500

    adding metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 import { Metadata } from "next";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-
-export const metadata: Metadata = {
-  title: "Consulta los cortes del servicio eléctrico",
-  description:
-    "Sistema de consulta de programación de suspensión de servicio eléctrico",
-};
+import { ExternalLink } from "lucide-react";
 
 export default async function Home() {
   return (
     <div className="py-48 flex items-center justify-center">
-      <Link href="/cnel">
-        <Button variant="outline">
-          Corporación Nacional de Electricidad (CNEL EP)
-        </Button>
-      </Link>
+      <div>
+        <div className="font-semibold mb-4">
+          Tenemos estas opciones disponibles:
+        </div>
+        <Link href="/cnel">
+          <Button variant="default">
+            <ExternalLink /> Corporación Nacional de Electricidad (CNEL EP)
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }

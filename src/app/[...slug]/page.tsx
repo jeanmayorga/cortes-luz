a43a217f8766ea7a60972df3ec4ccf9dc9fcaabc
Jean Paul Mayorga <jpmayorga@outlook.com>
2024-11-25 22:16:07 -0500

    adding metadata

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -1,11 +1,37 @@
 import { Accounts } from "@/components/Accounts";
 import { getAccounts } from "../config";
+import { Metadata } from "next";
 
 interface Params {
   slug: string[];
 }
 interface Props {
   params: Promise<Params>;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+  const [provider, criteria, code] = slug;
+
+  const accounts = await getAccounts({ provider, criteria, code });
+
+  const account = accounts[0];
+
+  if (account) {
+    return {
+      title: `${provider.toUpperCase()} | ${account.address.replace(
+        "...",
+        ""
+      )} | Cortes del servicio eléctrico`,
+      description: account.locations,
+    };
+  }
+
+  return {
+    title: "Consulta los cortes del servicio eléctrico.",
+    description:
+      "Sistema de consulta de programación de suspensión de servicio eléctrico",
+  };
 }
 
 export default async function Page({ params }: Props) {
